ee3915265c6aa1fa81ec9923cc55d449ea42a0c8
mhd <mhd@co>
2025-06-07 15:41:19 +0300

    Add a models/ExhibitorRequest

---
+++ models/ExhibitorRequest.js
@@ -0,0 +1,31 @@
+// models/ExhibitorRequest.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const ExhibitorRequest = sequelize.define('ExhibitorRequest', {
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  exhibitionName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  departmentId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: 'pending' // أو values مثل: pending / approved / rejected
+  },
+  contactPhone: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  notes: {
+    type: DataTypes.TEXT
+  }
+});
+
+module.exports = ExhibitorRequest;
