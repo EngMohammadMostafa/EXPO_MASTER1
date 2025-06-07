c50376bb4517758bc98d772f05695812370a5615
mhd <mhd@co>
2025-06-07 15:14:25 +0300

    Add a exhibitorRoutes

---
+++ routes/exhibitorRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const exhibitorController = require('../controllers/exhibitorController');
+const { verifyExhibitor } = require('../middleware/authMiddleware');
+
+// إنشاء طلب
+router.post('/create-request', verifyExhibitor, exhibitorController.createRequest);
+
+// دفع مبدئي
+router.post('/pay-initial', verifyExhibitor, exhibitorController.payInitial);
+
+// متابعة الطلب
+router.get('/track-request', verifyExhibitor, exhibitorController.trackRequest);
+
+// دفع نهائي
+router.post('/pay-final', verifyExhibitor, exhibitorController.payFinal);
+
+// إضافة المنتجات
+router.post('/add-products', verifyExhibitor, exhibitorController.addProducts);
+
+module.exports = router;
