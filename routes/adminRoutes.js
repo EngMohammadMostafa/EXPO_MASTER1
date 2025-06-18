ffd2b4b16bcf4857eedbae33f702de7cc45356bd
mhd <mhd@co>
2025-06-18 17:35:11 +0300

    update

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,22 +1,7 @@
-// const express = require('express');
-// const router = express.Router();
-
-// const { register } = require('../controllers/authController');
-// const { protect, authorize } = require('../middleware/authMiddleware');
-
-//  router.post('/create-manager', protect, authorize(4), register);
-
-// router.get('/dashboard', protect, authorize(4), (req, res) => {
-//   res.status(200).json({ message: 'لوحة تحكم مدير المعرض' });
-// });
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 
 const { register } = require('../controllers/authController');
-// استيراد verifyToken و authorize بالاسم الصحيح
 const { verifyToken, authorize } = require('../middleware/authMiddleware');
 
 router.post('/create-manager', verifyToken, authorize(4), register);
