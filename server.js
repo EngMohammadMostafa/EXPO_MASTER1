dfa497f96f1de18abc6e243db7835979e59cff6f
mhd <mhd@co>
2025-06-18 17:37:46 +0300

    update

--- server.js
+++ server.js
@@ -1,35 +1,4 @@
-// const express = require('express');
-// const cors = require('cors');
-// const sequelize = require('./config/db');
-// require('dotenv').config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// // ✅ مسارات التوجيه
-// const departmentRoutes = require('./routes/departmentRoutes');
-// const authRoutes = require('./routes/authRoutes');
-// const adminRoutes = require('./routes/adminRoutes');
-// const exhibitorRoutes = require('./routes/exhibitorRoutes');
-
-// app.use('/departments', departmentRoutes);
-// app.use('/auth', authRoutes);
-// app.use('/admin', adminRoutes);
-// app.use('/api/exhibitor', exhibitorRoutes);
-
-// const PORT = process.env.PORT || 3000;
-
-// sequelize.sync({ alter: true }).then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`✅ Server running on port ${PORT}`);
-//   });
-// }).catch((err) => {
-//   console.error('❌ Failed to connect to the database:', err.message);
-// });
-// server.js أو app.js
-const express = require('express');
+ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/db');
 require('dotenv').config();
