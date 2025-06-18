309f6456b546c500cc39abc354618f965f53036a
mhd <mhd@co>
2025-06-18 17:34:27 +0300

    update

--- models/User.js
+++ models/User.js
@@ -1,39 +1,13 @@
-// const { DataTypes } = require('sequelize');
-// const sequelize = require('../config/db');
 
-// const User = sequelize.define('User', {
-//   name: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   },
-//   email: {
-//     type: DataTypes.STRING,
-//     unique: true,
-//     allowNull: false
-//   },  
-//   password: {
-//     type: DataTypes.STRING
-//   },
-//   userType: {
-//     type: DataTypes.INTEGER, 
-//     allowNull: false,
-//     defaultValue: 1  
-//   }
-// });
-
-// // علاقته مع طلبات العارضين
-// const ExhibitorRequest = require('./ExhibitorRequest');
-// User.hasMany(ExhibitorRequest, { foreignKey: 'userId', as: 'requests' });
-
-// module.exports = User;
-// models/User.js
 const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/db');
 
 class User extends Model {}
 
 User.init({
-  name: { type: DataTypes.STRING, allowNull: false },
+  name: 
+  { 
+    type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, unique: true, allowNull: false },
   password: { type: DataTypes.STRING },
   userType: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 1 }
