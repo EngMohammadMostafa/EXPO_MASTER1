2b6af88ef50d45faa6c49f1c0f5127a0ded0d754
rebal <rebal.sh.the.dragon@gmail.com>
2025-06-18 06:02:39 -0700

    update

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { register } = require('../controllers/authController');
-const { protect, authorize } = require('../middleware/authMiddleware');
+// استيراد verifyToken و authorize بالاسم الصحيح
+const { verifyToken, authorize } = require('../middleware/authMiddleware');
 
- router.post('/create-manager', protect, authorize(4), register);
+router.post('/create-manager', verifyToken, authorize(4), register);
 
-router.get('/dashboard', protect, authorize(4), (req, res) => {
+router.get('/dashboard', verifyToken, authorize(4), (req, res) => {
   res.status(200).json({ message: 'لوحة تحكم مدير المعرض' });
 });
 
