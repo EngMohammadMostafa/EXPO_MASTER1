d335a92143b47b542f15144a5dd6e4474d60fb01
rebal <rebal.sh.the.dragon@gmail.com>
2025-06-18 07:34:07 -0700

    update

--- models/User.js
+++ models/User.js
@@ -1,24 +1,16 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/db');
 
-const User = sequelize.define('User', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false
-  },  
-  password: {
-    type: DataTypes.STRING
-  },
-  userType: {
-    type: DataTypes.INTEGER, 
-    allowNull: false,
-    defaultValue: 1  
-  }
+class User extends Model {}
+
+User.init({
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING },
+  userType: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 1 }
+}, {
+  sequelize,
+  modelName: 'User'
 });
 
 module.exports = User;
