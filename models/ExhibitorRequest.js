b68854171b123778320dce84991344461e0a5168
rebal <rebal.sh.the.dragon@gmail.com>
2025-06-18 07:33:24 -0700

    update

--- models/ExhibitorRequest.js
+++ models/ExhibitorRequest.js
@@ -1,29 +1,13 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/db');
-const User = require('./User');
-const Department = require('./Department');
-const Section = require('./Section');  
 
-const ExhibitorRequest = sequelize.define('ExhibitorRequest', {
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  exhibitionName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  departmentId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  contactPhone: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  notes: {
-    type: DataTypes.TEXT
-  },
+class ExhibitorRequest extends Model {}
+ExhibitorRequest.init({
+  userId: { type: DataTypes.INTEGER, allowNull: false },
+  exhibitionName: { type: DataTypes.STRING, allowNull: false },
+  departmentId: { type: DataTypes.INTEGER, allowNull: false },
+  contactPhone: { type: DataTypes.STRING, allowNull: false },
+  notes: { type: DataTypes.TEXT },
   status: {
     type: DataTypes.ENUM('pending', 'waiting-approval', 'approved', 'rejected'),
     defaultValue: 'pending'
@@ -36,27 +20,13 @@ const ExhibitorRequest = sequelize.define('ExhibitorRequest', {
     type: DataTypes.ENUM('unpaid', 'paid'),
     defaultValue: 'unpaid'
   },
-  finalPaymentDate: {
-    type: DataTypes.DATE,
-    allowNull: true
-  },
-  wingAssigned: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false
-  },
-  rejectionReason: {
-    type: DataTypes.TEXT,
-    allowNull: true
-  },
-  sectionId: {
-    type: DataTypes.INTEGER,
-    allowNull: true
-  }
+  finalPaymentDate: { type: DataTypes.DATE, allowNull: false   },
+  wingAssigned: { type: DataTypes.BOOLEAN, defaultValue: false },
+  rejectionReason: { type: DataTypes.TEXT, allowNull: true },
+  sectionId: { type: DataTypes.INTEGER, allowNull: false   }
+}, {
+  sequelize,
+  modelName: 'ExhibitorRequest'
 });
-
-
-ExhibitorRequest.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-ExhibitorRequest.belongsTo(Department, { foreignKey: 'departmentId', as: 'department' });
-ExhibitorRequest.belongsTo(Section, {foreignKey: 'sectionId',as: 'section'});
 
 module.exports = ExhibitorRequest;
