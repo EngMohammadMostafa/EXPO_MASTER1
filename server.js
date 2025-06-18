301c8c89a20bd852001f7e17a6951e014a78dd7f
rebal <rebal.sh.the.dragon@gmail.com>
2025-06-18 07:38:01 -0700

    update

--- server.js
+++ server.js
@@ -1,33 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const sequelize = require('./config/db');
-// require('dotenv').config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// // ✅ مسارات التوجيه
-// const departmentRoutes = require('./routes/departmentRoutes');
-// const authRoutes = require('./routes/authRoutes');
-// const adminRoutes = require('./routes/adminRoutes');
-// const exhibitorRoutes = require('./routes/exhibitorRoutes');
-
-// app.use('/departments', departmentRoutes);
-// app.use('/auth', authRoutes);
-// app.use('/admin', adminRoutes);
-// app.use('/api/exhibitor', exhibitorRoutes);
-
-// const PORT = process.env.PORT  3000;
-
-// sequelize.sync({ alter: true }).then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`✅ Server running on port ${PORT}`);
-//   });
-// }).catch((err) => {
-//   console.error('❌ Failed to connect to the database:', err.message);
-// });
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/db');
@@ -38,7 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ مسارات التوجيه
+// استيراد الموديلات (حتى يتم تعريفها)
+require('./models/User');
+require('./models/ExhibitorRequest');
+require('./models/Department');
+require('./models/Section');
+
+// استيراد العلاقات
+require('./models/associations');
+
+// استيراد الراوتس
 const departmentRoutes = require('./routes/departmentRoutes');
 const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
@@ -53,8 +32,8 @@ const PORT = process.env.PORT || 3000;
 
 sequelize.sync({ alter: true }).then(() => {
   app.listen(PORT, () => {
-    console.log(`✅ Server running on port ${PORT}`);
+    console.log(✅ Server running on port ${PORT});
   });
-}).catch((err) => {
+}).catch(err => {
   console.error('❌ Failed to connect to the database:', err.message);
 });
