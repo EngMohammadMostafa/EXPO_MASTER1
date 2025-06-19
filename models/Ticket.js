2fd7fce76553151b5f9e1887635eeb4d49734abf
rebal <rebal.sh.the.dragon@gmail.com>
2025-06-19 06:31:33 -0700

    create qr

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,36 +1,63 @@
-// models/Ticket.js
-const { DataTypes, Model } = require('sequelize');
-const sequelize = require('../config/db');  // تأكد أنك مهيأ اتصال Sequelize هنا
+// // models/Ticket.js
+// const { DataTypes, Model } = require('sequelize');
+// const sequelize = require('../config/db');  // تأكد أنك مهيأ اتصال Sequelize هنا
 
-class Ticket extends Model {}
+// class Ticket extends Model {}
 
-Ticket.init({
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  departmentId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  purchaseDate: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-  qrCode: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  status: {
-    type: DataTypes.STRING,
-    defaultValue: 'pending', // pending, paid, used
-  }
-}, {
-  sequelize,
-  modelName: 'Ticket',
-  tableName: 'tickets',
-  timestamps: false,
-});
+// Ticket.init({
+//   userId: {
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//   },
+//   departmentId: {
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//   },
+//   purchaseDate: {
+//     type: DataTypes.DATE,
+//     defaultValue: DataTypes.NOW,
+//   },
+//   qrCode: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//     unique: true,
+//   },
+//   status: {
+//     type: DataTypes.STRING,
+//     defaultValue: 'pending', // pending, paid, used
+//   }
+// }, {
+//   sequelize,
+//   modelName: 'Ticket',
+//   tableName: 'tickets',
+//   timestamps: false,
+// });
 
-module.exports = Ticket;
+// module.exports = Ticket;
+
+module.exports = (sequelize, DataTypes) => {
+  const Ticket = sequelize.define('Ticket', {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    departmentId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    qrCode: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: 'pending',
+    },
+    purchaseDate: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+
+  return Ticket;
+};
