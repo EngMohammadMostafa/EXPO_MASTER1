5ed5b5b1f388013b80da8a00e7f145d408713781
HaidarAlmoghrabi <mhhaidar69@gmail.com>
2025-06-20 23:52:27 +0300

    edit schedule.js to relationship with section

--- models/Schedule.js
+++ models/Schedule.js
@@ -1,8 +1,14 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const Department = require('./Department');
+const Section = require('./Section');
 
 const Schedule = sequelize.define('Schedule', {
   departmentId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  sectionId: {
     type: DataTypes.INTEGER,
     allowNull: false,
   },
@@ -16,8 +22,7 @@ const Schedule = sequelize.define('Schedule', {
   },
 });
 
-// تعريف العلاقة هنا 
-const Department = require('./Department');
 Schedule.belongsTo(Department, { foreignKey: 'departmentId', as: 'department' });
+Schedule.belongsTo(Section, { foreignKey: 'sectionId', as: 'section' });
 
 module.exports = Schedule;
