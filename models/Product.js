e6de841afe2f9eb5cd7a83cbf813beccd7dbd6fb
HaidarAlmoghrabi <mhhaidar69@gmail.com>
2025-06-22 16:20:59 +0300

    Define Product model with associations to User and Section models

--- models/Product.js
+++ models/Product.js
@@ -1,26 +1,33 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
-const User = require('./User');
+const { DataTypes } = require('sequelize');   // استيراد أنواع البيانات من Sequelize
+const sequelize = require('../config/db');    // استيراد إعداد اتصال قاعدة البيانات
+const User = require('./User');                // استيراد موديل المستخدم
 
+// تعريف موديل المنتج
 const Product = sequelize.define('Product', {
-  exhibitorId: {
+  exhibitorId: {                             // معرف العارض (مالك المنتج)
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false                         // مطلوب
   },
-   sectionId: {
+  sectionId: {                              // معرف الجناح (القسم) الذي ينتمي له المنتج
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false                         // مطلوب
   },
-  productName: {
+  productName: {                            // اسم المنتج
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false                         // مطلوب
   },
-  description: {
+  description: {                            // وصف المنتج (اختياري)
     type: DataTypes.TEXT
   },
-  price: {
+  price: {                                 // سعر المنتج (اختياري)
     type: DataTypes.FLOAT
   }
 });
+
+// إنشاء علاقة belongsTo مع الموديل User باستخدام مفتاح أجنبي exhibitorId
 Product.belongsTo(User, { foreignKey: 'exhibitorId', as: 'exhibitor' });
+
+// إنشاء علاقة belongsTo مع موديل Section باستخدام مفتاح أجنبي sectionId
 Product.belongsTo(require('./Section'), { foreignKey: 'sectionId', as: 'section' });
+
+module.exports = Product;  // تصدير موديل المنتج للاستخدام في باقي أجزاء التطبيق
