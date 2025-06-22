ab231c912d164ec3a51a02d179964696d93f92c1
mhd <mhd@co>
2025-06-22 17:59:51 +0300

    update

--- models/Section.js
+++ models/Section.js
@@ -1,47 +1,4 @@
-// const { DataTypes } = require('sequelize');
-// const sequelize = require('../config/db');
-// const Department = require('./Department');
-// const User = require('./User');
-
-// const Section = sequelize.define('Section', {
-//   name: {
-//     type: DataTypes.STRING(35),
-//     allowNull: false,
-//   },
-//   departments_id: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false,
-//   },
-//   exhibitor_id: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false,
-//   }
-// });
-
-// // إذا فيه علاقات لاحقًا ممكن إضافتها هون
-
-// module.exports = Section;
-
-// // const { DataTypes } = require('sequelize');
-// // const sequelize = require('../config/db');
-// // const Department = require('./Department');
-// // const User = require('./User');
-// // const Section = sequelize.define('Section', {
-// //   name: {
-// //     type: DataTypes.STRING(35),
-// //     allowNull: false,
-// //   },
-// //   departments_id: {
-// //     type: DataTypes.INTEGER,
-// //     allowNull: false,
-// //   },
-// //   exhibitor_id: {
-// //     type: DataTypes.INTEGER,
-// //     allowNull: false,
-// //   },
-// // });
-// // module.exports = Section;
-const { DataTypes } = require('sequelize');
+ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Section = sequelize.define('Section', {
@@ -51,4 +8,3 @@ const Section = sequelize.define('Section', {
 });
 
 module.exports = Section;
-  
--- models/User.js
+++ models/User.js
@@ -1,21 +1,3 @@
-// const { DataTypes, Model } = require('sequelize');
-// const sequelize = require('../config/db');
-
-// class User extends Model {}
-
-// User.init({
-//   name: 
-//   { 
-//     type: DataTypes.STRING, allowNull: false },
-//   email: { type: DataTypes.STRING, unique: true, allowNull: false },
-//   password: { type: DataTypes.STRING },
-//   userType: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 1 }
-// }, {
-//   sequelize,
-//   modelName: 'User'
-// });
-
-// module.exports = User;
 const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/db');
 
