ab9f33a3523a37d315c055edb7bfb7dd0599246a
HaidarAlmoghrabi <mhhaidar69@gmail.com>
2025-06-22 16:30:16 +0300

    edit departmentRoutes.js

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -9,14 +9,23 @@ const {
   getAllDepartments,
 } = require('../controllers/departmentController');
 
-// ✅ حماية جميع المسارات والتأكد من أن المستخدم مدير مدينة (userType = 4)
+// ✅ حماية جميع المسارات: التحقق من التوكن والتأكد من أن المستخدم هو مدير المدينة (userType = 4)
 router.use(verifyToken);
 router.use(authorize(4));
 
+// جلب جميع الأقسام
 router.get('/', getAllDepartments);
+
+// إنشاء قسم جديد
 router.post('/', createDepartment);
+
+// جلب قسم معين عبر المعرف
 router.get('/:id', getDepartmentById);
+
+// تحديث قسم معين
 router.put('/:id', updateDepartment);
+
+// حذف قسم معين
 router.delete('/:id', deleteDepartment);
 
 module.exports = router;
