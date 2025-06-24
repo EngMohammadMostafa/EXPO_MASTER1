7172c4c8eac64cfd5ce0731867d20fbbd5c5afb3
rebal <rebal.sh.the.dragon@gmail.com>
2025-06-24 08:16:24 -0700

    update

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/db');
-require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// استيراد الموديلات (حتى يتم تعريفها)
+// استيراد الموديلات
 require('./models/User');
-require('./models/ExhibitorRequest');
 require('./models/Department');
 require('./models/Section');
-
+require('./models/ExhibitorRequest');
+require('./models/Product');
 // استيراد العلاقات
-require('./models/associations');
+require('./models');
 
 // استيراد الراوتس
 const departmentRoutes = require('./routes/departmentRoutes');
 const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const exhibitorRoutes = require('./routes/exhibitorRoutes');
-const ticketRoutes = require('./routes/ticketRoutes');
+const departmentManagerRoutes = require('./routes/departmentManagerRoutes');
+const visitorRoutes = require('./routes/visitorRoutes');
 
-app.use('/tickets', ticketRoutes);
+// ربط الراوتس
 app.use('/departments', departmentRoutes);
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/api/exhibitor', exhibitorRoutes);
+app.use('/api/department-manager', departmentManagerRoutes);
+app.use('/api/visitor', visitorRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync({ alter: true }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`✅ Server running on port ${PORT}`);
+sequelize.sync().then(() => {
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(✅ Server running on port ${PORT});
   });
 }).catch(err => {
-  console.error('❌ Failed to connect to the database:', err.message);
+  console.error('❌ Failed to connect:', err.message);
 });
