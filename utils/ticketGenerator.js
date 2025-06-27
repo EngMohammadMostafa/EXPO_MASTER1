c6f21d404be079082aa43bb790c48c0b93e34845
rebal <rebal.sh.the.dragon@gmail.com>
2025-06-27 10:12:04 -0700

    update

--- utils/ticketGenerator.js
+++ utils/ticketGenerator.js
@@ -4,16 +4,16 @@ const path = require('path');
 
 exports.generateTicket = async (visitor) => {
   const doc = new PDFDocument();
-  const filePath = path.join(__dirname, ../tickets/ticket_${visitor.id}.pdf);
+  const filePath = path.join(`ذ__dirname, ../tickets/ticket_${visitor.id}.pdf`);
   const stream = fs.createWriteStream(filePath);
 
   doc.pipe(stream);
   doc.fontSize(20).text('🎫 Exhibition City Ticket', { align: 'center' });
   doc.moveDown();
-  doc.fontSize(14).text(Name: ${visitor.name});
-  doc.fontSize(14).text(Email: ${visitor.email});
-  doc.fontSize(14).text(User ID: ${visitor.id});
+  doc.fontSize(14).text(`Name: ${visitor.name}`);
+  doc.fontSize(14).text(`Email: ${visitor.email}`);
+  doc.fontSize(14).text(`User ID: ${visitor.id}`);
   doc.moveDown();
-  doc.fontSize(12).text(Date Issued: ${new Date().toLocaleString()});
+  doc.fontSize(12).text(Date `Issued: ${new Date().toLocaleString()}`);
   doc.end();
 };
